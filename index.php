85004a43f54f3d4faf9c2e0625ed2bd9db7d98f7
Mickael <mickael.saidi1@gmail.com>
2018-02-19 11:28:49 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Javascript</title>
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/fontawesome.min.css" />
+    <link rel="stylesheet" ref="css/tether.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/bootstrap-reboot.css">
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/tether.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="main.js"></script>
+</head>
+<body>
+
+<h1>ZOOOO</h1>
+
+</body>
+</html>
